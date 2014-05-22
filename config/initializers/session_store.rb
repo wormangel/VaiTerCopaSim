f8e84d5c175261ec57316aa34a0bad0adbb73759
wormangel <wormangel@gmail.com>
2014-05-22 18:02:28 -0300

    changed session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_troca-copa_session'
+Rails.application.config.session_store :cookie_store, key: '_nao_vai_ter_copa_session'
