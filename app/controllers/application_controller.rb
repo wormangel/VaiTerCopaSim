940e4e722a6bea870ac4fd1ade7a7821cc631260
wormangel <wormangel@gmail.com>
2014-05-23 03:18:54 -0300

    helper method for profile pic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,5 +12,11 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+  
+  def profile_pic(size = 'square')
+  	@profile_pic = 'http://graph.facebook.com/'+ current_user.uid + "/picture?type=" + size
+  end
+  
   helper_method :current_user
+  helper_method :profile_pic
 end
