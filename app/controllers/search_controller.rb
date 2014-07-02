1ae3b0b6968109f4916872a6bc553f9e3892d140
Otacilio Lacerda <otacilio@Otacilios-MacBook-Pro.local>
2014-07-01 20:12:15 -0500

    Clean search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,18 +1,13 @@
 class SearchController < ApplicationController
   def users
-  	#users = User.where.not(uid: current_user.uid)
-  	
   	@users_data = []
-
-	#current_user_dupes = DuplicateSticker.joins(:sticker).where(user: current_user).order('stickers.order')
-	#current_user_missing = NeededSticker.joins(:sticker).where(user: current_user).order('stickers.order')
 
 	list = DuplicateSticker.where.not(user: current_user).where(sticker: NeededSticker.select('sticker_id').where(user: current_user)).group(:user).order('count_all DESC').count()
 
   	list.keys.each do |user|
   		
   		u_data = Hash.new
-  		
+
 		u_data["uid"] = user.uid
 		u_data["name"] = user.name
 		u_data['dupe_count'] = list[user]
